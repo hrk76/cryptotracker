3e9fa79e7f344812254f702144d9fbba79723613
hrk76 <austinsong56@gmail.com>
2022-09-09 15:19:02 -0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div>
+    <Header/>
+    <Route/>
+    
+    </div>
+      
+    </BrowserRouter>
+  );
+}
+
+export default App;
