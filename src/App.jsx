bacdbb8953240f049c2fb2a5e6a42208e09d3a1b
hrk76 <austinsong56@gmail.com>
2022-09-12 11:02:12 -0700

    more pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import CoinPage from "./Pages/CoinPage";
+import Homepage from "./Pages/Homepage";
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-    <Header/>
-    <Route/>
-    
-    </div>
-      
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} exact />
+          <Route path="/coins/id" element={<CoinPage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
